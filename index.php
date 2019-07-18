f0bde96195b724005787880b73b4d6caebfd6b21
Eduado Tupan <eduardotupantupan@gmail.com>
2019-07-18 00:23:18 -0300

    commit unico

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>contato</title>
+</head>
+<body>
+    <form name="contato" method="post" action="enviar.php">
+
+        nome: <input type="text" name="nome"> <br>
+        E-mail: <input type="text" name="email"> <br>
+        assunto: <input type="text" name="assunto"> <br>
+        Mensagem: <textarea name="mensagem"></textarea>
+        <input type="submit" value="Enviar"> 
+    </form>
+</body>
+</html>
